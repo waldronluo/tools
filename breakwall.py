d4543ba65a5790017d8f35d7e29a5c4ec2a11c6b
waldron <waldron@5750G>
2013-07-03 01:43:44 -0700

    ver 0.0.2

--- breakwall.py
+++ breakwall.py
@@ -2,4 +2,4 @@ import subprocess
 
 if __name__ == "__main__":
     while (True):
-        subprocess.call(['plink','-N','-v','-P','','@suzaku.tk','-D','127.0.0.1:7070','-pw','']);
+        subprocess.call(['ssh','-qTnN','-D','7070','waldronluo@suzaku.tk']);
